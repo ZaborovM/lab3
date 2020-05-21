7262fcc7da6748763519db2a6cb7f721e2ebafda
ZaborovM <63392562+ZaborovM@users.noreply.github.com>
2020-05-21 12:36:18 +0300

    Add files via upload

---
+++ Zaborov.bat
@@ -0,0 +1,27 @@
+Echo off
+md Bernemann
+cd Bernemann
+md Claus
+cd Claus
+md Shulz
+cd ..
+pause
+echo > 100500.txt
+cd Claus
+echo > 131414.txt
+cd Shulz
+echo > 3456563.txt
+cd ..
+cd ..
+cd ..
+pause
+del Bernemann /S /Q /F
+pause
+cd Bernemann
+cd Claus
+rd Shulz
+cd ..
+rd Claus
+cd ..
+rd Bernemann
+pause
